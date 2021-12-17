fbff9f9fa72266dc42727ea3203c47956d1947f9
Hemanth0304 <sharkatocean@gmail.com>
2021-12-17 11:25:51 +0530

    Todo List Backend Node

---
+++ controller/todo.controller.js
@@ -0,0 +1,151 @@
+const db = require("../models");
+const Tutorial = db.tutorials;
+
+// Create and Save a new Task
+exports.create = (req, res) => {
+  // Validate request
+  if (!req.body.title) {
+    res.status(400).send({ message: "Content can not be empty!" });
+    return;
+  }
+
+  // Create a Task
+  const tutorial = new Tutorial({
+    title: req.body.title,
+    description: req.body.description,
+    today:req.body.today,
+    Status: req.body.Status ? req.body.Status : false
+  });
+
+  // Save Task in the database
+  tutorial
+    .save(tutorial)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the Task."
+      });
+    });
+};
+
+// Retrieve all Tasks from the database.
+exports.findAll = (req, res) => {
+  const title = req.query.title;
+  var condition = title ? { title: { $regex: new RegExp(title), $options: "i" } } : {};
+
+  Tutorial.find(condition)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving tasks."
+      });
+    });
+};
+
+// Find a single Task with an id
+exports.findOne = (req, res) => {
+  const id = req.params.id;
+
+  Tutorial.findById(id)
+    .then(data => {
+      if (!data)
+        res.status(404).send({ message: "Not found Task with id " + id });
+      else res.send(data);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .send({ message: "Error retrieving Task with id=" + id });
+    });
+};
+
+// Update a Task by the id in the request
+exports.update = (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({
+      message: "Data to update can not be empty!"
+    });
+  }
+
+  const id = req.params.id;
+
+  Tutorial.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot update Task with id=${id}. Maybe Task was not found!`
+        });
+      } else res.send({ message: "Task was updated successfully." });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating Task with id=" + id
+      });
+    });
+};
+
+
+
+
+// Delete a Task with the specified id in the request
+exports.delete = (req, res) => {
+  const id = req.params.id;
+
+  Tutorial.findByIdAndRemove(id, { useFindAndModify: false })
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot delete Task with id=${id}. Maybe Task was not found!`
+        });
+      } else {
+        res.send({
+          message: "Task was deleted successfully!"
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Could not delete Task with id=" + id
+      });
+    });
+};
+
+
+
+
+
+// Delete all Tasks from the database.
+exports.deleteAll = (req, res) => {
+  Tutorial.deleteMany({})
+    .then(data => {
+      res.send({
+        message: `${data.deletedCount} Tasks were deleted successfully!`
+      });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all tasks."
+      });
+    });
+};
+
+// Find all Done Tasks
+exports.findAllPublished = (req, res) => {
+  Tutorial.find({ Status: true })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving task."
+      });
+    });
+};
